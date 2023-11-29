b42f1b21288b4c7e59984388287498a459036be5
julianstraif <julian.straif@gmx.de>
2023-11-29 14:19:55 +0100

    added simple Homescreen with Foxes

---
+++ my-app/src/pages/Home/Home.js
@@ -0,0 +1,23 @@
+import React from "react";
+import {textAlign} from "@mui/system";
+
+function Home() {
+    return (
+        <div>
+            <h1 style={{ textAlign: "center" }}>Spuren im Netz</h1>
+            <div style={{ display: "flex", justifyContent: "center" }}>
+                <p>
+                    Willkommen!
+                </p>
+            </div>
+            <div style={{ display: "flex", justifyContent: "center" }}>
+                <img src="/images/EvilFox.png" alt="Fuchs" style={{ position: "absolute", bottom: 0, marginLeft: 500 }} />
+                <img src="/images/GoodFox.png" alt="Fuchs" style={{ position: "absolute", bottom: 0, marginRight: 500}} />
+
+            </div>
+
+        </div>
+    );
+}
+
+export default Home;
