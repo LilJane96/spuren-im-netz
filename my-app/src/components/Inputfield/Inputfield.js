981c67ba5cb56ebe0d4fd4b330ad547355284652
julianstraif <julian.straif@gmx.de>
2024-02-10 12:09:43 +0100

    add readOnly to inputfield

--- my-app/src/components/Inputfield/Inputfield.js
+++ my-app/src/components/Inputfield/Inputfield.js
@@ -4,7 +4,7 @@ import "./Inputfield.css"
 export default function Inputfield({placeholder, value, onChange, type}) {
     return (
         <div className="InputfieldContainer">
-           <input type={type} className="inputfieldComponent" placeholder={placeholder} value={value}  onChange={onChange}></input>
+           <input type={type} className="inputfieldComponent" placeholder={placeholder} value={value} readOnly={true} onChange={onChange}></input>
         </div>
     )
 }
