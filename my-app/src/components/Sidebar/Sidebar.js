a5282d2e62b7d7e531f9cb0e0ed4971dffbac51c
julianstraif <julian.straif@gmx.de>
2024-02-10 12:04:28 +0100

    add disclaimer to rules

--- my-app/src/components/Sidebar/Sidebar.js
+++ my-app/src/components/Sidebar/Sidebar.js
@@ -18,6 +18,9 @@ export default function Sidebar({toggleSidebarVisibility }) {
                     <li>Wenn du an einer Stelle nicht weiter kommst, frage bitte deinen Lehrer oder deine Lehrerin nach Hilfe!</li>
                 </ul>
                 <ColorContainer headingsize={"h6"}/>
+                <div className="disclaimer">
+                <p>Disclaimer: Diese Applikation wurde für die Auflösung 1366x617 konzipiert und programmiert.</p>
+                </div>
             </div>
         </div>
     );
