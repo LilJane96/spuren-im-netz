4eb9377443b72e394dcbfdbe698304d5f0b08990
Julian <js437@hdm-stuttgart.de>
2024-07-14 12:46:40 +0200

    add wrong answer test and button disabled test

--- tests/unit1.spec.ts
+++ tests/unit1.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('unit1_test', async ({ page }) => {
-  
+
   await page.goto('/hub');
   await page.getByRole('img', { name: 'Pin1' }).click();
   await page.getByPlaceholder('Name').fill('Julian');
@@ -28,3 +28,78 @@ test('unit1_test', async ({ page }) => {
   
 
 });
+
+    // wrong answer test
+    // button has attribute disabled if answer is wrong otherwise not
+
+    test('unit1_test_wrong_answer', async ({ page }) => {
+
+      await page.goto('http://localhost:3000/frameone/unit1/step1');
+      const continueButton = await page.getByRole('button', { name: 'Weiter' });
+
+      await page.getByText('Name von meinem Opa').click();
+      await expect(continueButton).toHaveAttribute('disabled');
+
+      await page.goto('http://localhost:3000/frameone/unit1/step2');
+
+      await page.getByText('Passwort', { exact: true }).click();
+      await expect(continueButton).toHaveAttribute('disabled');
+
+      await page.goto('http://localhost:3000/frameone/unit1/step3');
+
+      await page.getByText('Öffentlich', { exact: true }).click();
+      await expect(continueButton).toHaveAttribute('disabled');
+
+      await page.goto('http://localhost:3000/frameone/unit1/step4');
+
+      await page.getByRole('img', { name: 'Bildbeschreibung' }).nth(2).click();
+      await expect(continueButton).toHaveAttribute('disabled');
+
+      await page.goto('http://localhost:3000/frameone/unit1/step5');
+
+      await page.getByText('Alter und Adresse von meiner').click();
+      const endUnit = await page.getByRole('button', { name: 'Level beenden' });
+      await expect(endUnit).toHaveAttribute('disabled');
+
+  });
+
+
+// button enabled - disabled test
+
+    test('unit1_test_wrong_answer_button', async ({ page }) => {
+
+      await page.goto('http://localhost:3000/frameone/unit1/step1');
+    const continueButton = await page.getByRole('button', { name: 'Weiter' });
+
+    await page.getByText('Name von meinem Opa').click();
+    await expect(continueButton).toHaveAttribute('disabled');
+    await page.getByText('Puzzlestar').click();
+    await expect(continueButton).not.toHaveAttribute('disabled');
+
+    await page.goto('http://localhost:3000/frameone/unit1/step2');
+
+    await page.getByText('Passwort', { exact: true }).click();
+    await expect(continueButton).toHaveAttribute('disabled');
+    await page.getByText('kYh5&0!mlta').click();
+    await expect(continueButton).not.toHaveAttribute('disabled');
+
+    await page.goto('http://localhost:3000/frameone/unit1/step3');
+
+    await page.getByText('Öffentlich', { exact: true }).click();
+    await expect(continueButton).toHaveAttribute('disabled');
+    await page.getByText('Privat', { exact: true }).click();
+    await expect(continueButton).not.toHaveAttribute('disabled');
+
+    await page.goto('http://localhost:3000/frameone/unit1/step4');
+
+    await page.getByRole('img', { name: 'Bildbeschreibung' }).nth(2).click();
+    await expect(continueButton).toHaveAttribute('disabled');
+    await page.getByRole('img', { name: 'Bildbeschreibung' }).first().click();
+    await expect(continueButton).not.toHaveAttribute('disabled');
+
+    await page.goto('http://localhost:3000/frameone/unit1/step5');
+    await page.getByText('Alter und Adresse von meiner').click();
+    const endUnit = await page.getByRole('button', { name: 'Level beenden' });
+    await expect(endUnit).toHaveAttribute('disabled');
+
+    });
--- tests/unit2.spec.ts
+++ tests/unit2.spec.ts
@@ -26,8 +26,88 @@ test('unit2_test', async ({ page }) => {
     await page.getByRole('button', { name: 'Weiter' }).click();
     await page.getByRole('button', { name: 'Weiter' }).click();
     await page.getByRole('img', { name: 'ClosedBox' }).click();
-    
+
     await expect(page.getByRole('button', { name: 'Schau dir deine Ergebnisse an!' })).toHaveCount(1);
 
 
   });
+
+    // wrong answer test
+    // button has attribute disabled if answer is wrong otherwise not
+
+  test('unit2_test_wrong_answer', async ({ page }) => {
+
+    await page.goto('http://localhost:3000/frameone/unit2/step1');
+    const continueButton = await page.getByRole('button', { name: 'Weiter' });
+
+    await page.getByText('Annehmen').click();
+    await expect(continueButton).toHaveAttribute('disabled');
+
+    await page.goto('http://localhost:3000/frameone/unit2/step2');
+
+    await page.getByText('Felicia29').nth(1).click();
+    await expect(continueButton).toHaveAttribute('disabled');
+
+    await page.goto('http://localhost:3000/frameone/unit2/step3');
+
+    await page.getByText('Berlin', { exact: true }).click();
+    await expect(continueButton).toHaveAttribute('disabled');
+
+    await page.goto('http://localhost:3000/frameone/unit2/step4');
+
+    await page.getByText('19').click();
+    await expect(continueButton).toHaveAttribute('disabled');
+
+    await page.goto('http://localhost:3000/frameone/unit2/step5');
+
+    await page.getByText('Lehrerin').click();
+    const endUnit = await page.getByRole('button', { name: 'Level beenden' });
+    await expect(endUnit).toHaveAttribute('disabled');
+
+});
+
+
+// button enabled - disabled test
+
+test('unit2_test_wrong_answer_button', async ({ page }) => {
+
+    await page.goto('http://localhost:3000/frameone/unit2/step1');
+    const continueButton = await page.getByRole('button', { name: 'Weiter' });
+    
+    await page.getByText('Annehmen').click();
+    await expect(continueButton).toHaveAttribute('disabled');
+    
+    await page.locator('div').filter({ hasText: /^Ablehnen$/ }).nth(1).click();
+    await expect(continueButton).not.toHaveAttribute('disabled');
+    
+    
+    await page.goto('http://localhost:3000/frameone/unit2/step2');
+
+    await page.getByText('Felicia29').nth(1).click();
+    await expect(continueButton).toHaveAttribute('disabled');
+    await page.getByText('Felix').nth(4).click();
+    await expect(continueButton).not.toHaveAttribute('disabled');
+
+    await page.goto('http://localhost:3000/frameone/unit2/step3');
+
+    await page.getByText('Berlin', { exact: true }).click();
+    await expect(continueButton).toHaveAttribute('disabled');
+    await page.getByText('Reutlingen', { exact: true }).click();
+    await expect(continueButton).not.toHaveAttribute('disabled');
+
+    await page.goto('http://localhost:3000/frameone/unit2/step4');
+
+    await page.getByText('19').click();
+    await expect(continueButton).toHaveAttribute('disabled');
+    await page.locator('div').filter({ hasText: /^39$/ }).nth(1).click();
+    await expect(continueButton).not.toHaveAttribute('disabled');
+
+    await page.goto('http://localhost:3000/frameone/unit2/step5');
+
+    await page.getByText('Lehrerin').click();
+    const endUnit = await page.getByRole('button', { name: 'Level beenden' });
+    await expect(endUnit).toHaveAttribute('disabled');
+    await page.getByText('Dieb').click();
+    await expect(endUnit).not.toHaveAttribute('disabled');
+
+    });
