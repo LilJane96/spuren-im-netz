6a93c195e93aeafdba86da4cbd3c49abb5ba7386
Liljana Stefanelli <ls193@hdm-stuttgart.de>
2024-08-17 17:34:49 +0200

    add mobile view at hub and frameOne

--- my-app/src/components/SwitchComponent/SwitchComponent.js
+++ my-app/src/components/SwitchComponent/SwitchComponent.js
@@ -1,73 +1,70 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Switch from '@mui/material/Switch';
-import FormControlLabel from '@mui/material/FormControlLabel';
-
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Switch from "@mui/material/Switch";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import "./SwitchComponent.css";
 
 const IOSSwitch = styled((props) => (
-    <Switch focusVisibleClassName=".Mui-focusVisible" disableRipple {...props} />
-  ))
-  (({ theme }) => ({
-
-    width: 42,
-    height: 26,
-    border: '1px solid black',
-    borderRadius: '30px',
+  <Switch focusVisibleClassName=".Mui-focusVisible" disableRipple {...props} />
+))(({ theme }) => ({
+  width: 42,
+  height: 26,
+  border: "1px solid black",
+  borderRadius: "30px",
+  padding: 0,
+  "& .MuiSwitch-switchBase": {
     padding: 0,
-    '& .MuiSwitch-switchBase': {
-      padding: 0,
-      margin: 1,
-      color: '#1976d2',
-      transitionDuration: '300ms',
-      '&.Mui-checked': {
-        transform: 'translateX(16px)',
-        color: '#fff',
-        '& + .MuiSwitch-track': {
-          backgroundColor: "#00A6FB",
-          opacity: 1,
-          border: 0,
-        },
-        '&.Mui-disabled + .MuiSwitch-track': {
-          opacity: 0.5,
-        },
+    margin: 1,
+    color: "#1976d2",
+    transitionDuration: "300ms",
+    "&.Mui-checked": {
+      transform: "translateX(16px)",
+      color: "#fff",
+      "& + .MuiSwitch-track": {
+        backgroundColor: "#00A6FB",
+        opacity: 1,
+        border: 0,
       },
-      '&.Mui-focusVisible .MuiSwitch-thumb': {
-        color: '#33cf4d',
-        border: '6px solid #fff',
-      },
-      '&.Mui-disabled .MuiSwitch-thumb': {
-        color:
-          theme.palette.mode === 'light'
-            ? theme.palette.grey[100]
-            : theme.palette.grey[600],
-      },
-      '&.Mui-disabled + .MuiSwitch-track': {
-        opacity: theme.palette.mode === 'light' ? 0.7 : 0.3,
+      "&.Mui-disabled + .MuiSwitch-track": {
+        opacity: 0.5,
       },
     },
-    '& .MuiSwitch-thumb': {
-      boxSizing: 'border-box',
-      width: 22,
-      height: 22,
+    "&.Mui-focusVisible .MuiSwitch-thumb": {
+      color: "#33cf4d",
+      border: "6px solid #fff",
     },
-    '& .MuiSwitch-track': {
-      borderRadius: 26 / 2,
-      backgroundColor: "white",
-      opacity: 1,
-      transition: theme.transitions.create(['background-color'], {
-        duration: 500,
-      }),
+    "&.Mui-disabled .MuiSwitch-thumb": {
+      color:
+        theme.palette.mode === "light"
+          ? theme.palette.grey[100]
+          : theme.palette.grey[600],
     },
-  }));
+    "&.Mui-disabled + .MuiSwitch-track": {
+      opacity: theme.palette.mode === "light" ? 0.7 : 0.3,
+    },
+  },
+  "& .MuiSwitch-thumb": {
+    boxSizing: "border-box",
+    width: 22,
+    height: 22,
+  },
+  "& .MuiSwitch-track": {
+    borderRadius: 26 / 2,
+    backgroundColor: "white",
+    opacity: 1,
+    transition: theme.transitions.create(["background-color"], {
+      duration: 500,
+    }),
+  },
+}));
 
-
-  export default function SwitchComponent({initialState}) {
-    return (
-        <div>
-            <FormControlLabel
-            control={<IOSSwitch sx={{ m: 1 }} />}
-            checked={initialState}
-            />
-        </div>
-    )
-  }
+export default function SwitchComponent({ initialState }) {
+  return (
+    <div>
+      <FormControlLabel
+        control={<IOSSwitch sx={{ m: 1 }} />}
+        checked={initialState}
+      />
+    </div>
+  );
+}
